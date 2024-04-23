d2284d45f3e434363cf58c888e299fde0420a3e2
INFORMATICA\dam_hsk <k4631415@gmail.com>
2024-04-23 13:15:31 +0200

    Commit con 15 test

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,9 +3,9 @@ public class FizzBuzz {
         int devuelveFizz = num % 3;
         int devuelveBuzz = num % 5;
         String cadena = String.valueOf(num);
+        if (devuelveFizz == 0 && devuelveBuzz == 0) return "FizzBuzz";
         if (devuelveFizz == 0) return "Fizz";
         if (devuelveBuzz == 0) return "Buzz";
-        if (devuelveFizz == 0 && devuelveBuzz == 0) return "FizzBuzz";
         return cadena;
     }
 }
--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -47,4 +47,34 @@ public class FizzBuzzTest {
     public void testConElNumero_9(){
         assertEquals("Fizz", FizzBuzz.comprobarNumero(9));
     }
+
+    @Test
+    public void testConElNumero_10(){
+        assertEquals("Buzz", FizzBuzz.comprobarNumero(10));
+    }
+
+    @Test
+    public void testConElNumero_11(){
+        assertEquals("11", FizzBuzz.comprobarNumero(11));
+    }
+
+    @Test
+    public void testConElNumero_12(){
+        assertEquals("Fizz", FizzBuzz.comprobarNumero(12));
+    }
+
+    @Test
+    public void testConElNumero_13(){
+        assertEquals("13", FizzBuzz.comprobarNumero(13));
+    }
+
+    @Test
+    public void testConElNumero_14(){
+        assertEquals("14", FizzBuzz.comprobarNumero(14));
+    }
+
+    @Test
+    public void testConElNumero_15() {
+        assertEquals("FizzBuzz", FizzBuzz.comprobarNumero(15));
+    }
 }
