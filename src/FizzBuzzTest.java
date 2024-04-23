906ce5e4772355a0777ee14977f70ab6ab0f12a7
INFORMATICA\dam_hsk <k4631415@gmail.com>
2024-04-23 13:01:41 +0200

    Tercer commit con 9 test

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -17,4 +17,34 @@ public class FizzBuzzTest {
     public void testConElNumero_3(){
         assertEquals("Fizz", FizzBuzz.comprobarNumero(3));
     }
+
+    @Test
+    public void testConElNumero_4(){
+        assertEquals("4", FizzBuzz.comprobarNumero(4));
+    }
+
+    @Test
+    public void testConElNumero_5(){
+        assertEquals("Buzz", FizzBuzz.comprobarNumero(5));
+    }
+
+    @Test
+    public void testConElNumero_6(){
+        assertEquals("Fizz", FizzBuzz.comprobarNumero(6));
+    }
+
+    @Test
+    public void testConElNumero_7(){
+        assertEquals("7", FizzBuzz.comprobarNumero(7));
+    }
+
+    @Test
+    public void testConElNumero_8(){
+        assertEquals("8", FizzBuzz.comprobarNumero(8));
+    }
+
+    @Test
+    public void testConElNumero_9(){
+        assertEquals("Fizz", FizzBuzz.comprobarNumero(9));
+    }
 }
